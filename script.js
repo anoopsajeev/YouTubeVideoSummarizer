6ccf4487692de6bf2068084097168ee9c53784d1
anoopsajeev <166618392+anoopsajeev@users.noreply.github.com>
2024-04-13 10:49:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function summarizeVideo() {
     // Show loading message
     document.getElementById("loading").style.display = "block";
 
-    fetch('http://127.0.0.2:6000/summarize', {
+    fetch('http://127.0.0.1:5000/summarize', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
